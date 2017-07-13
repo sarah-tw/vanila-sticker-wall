c9badb5cb532c6f096bc2b21e61e9a67aa9de010
Sarah <xiaojing.si@rea-group.com>
2017-07-14 01:07:47 +0800

    add dist folder for gh-pages branch

---
+++ dist/jss/main.js
@@ -0,0 +1,12 @@
+"use strict";
+
+var _jquery = require("jquery");
+
+var _jquery2 = _interopRequireDefault(_jquery);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+(function () {
+	console.log("hello");
+	console.log("hello world");
+})();
